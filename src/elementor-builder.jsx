1841f9fff2119b20ac13e1ddfe8fd7d8ade080a4
sectacularcode <admin@editorialvibes.com>
2026-06-19 17:35:33 -0400

    Add file-text icon to UI_ICONS for Export Brief button

--- src/elementor-builder.jsx
+++ src/elementor-builder.jsx
@@ -24,6 +24,7 @@ const UI_ICONS = {
   x: (k) => [<line key={k+"a"} x1="18" y1="6" x2="6" y2="18" />, <line key={k+"b"} x1="6" y1="6" x2="18" y2="18" />],
   pin: (k) => [<line key={k+"a"} x1="12" y1="17" x2="12" y2="22" />, <path key={k+"b"} d="M5 17h14V8H5z" />, <path key={k+"c"} d="M9 8V4h6v4" />],
   edit: (k) => [<path key={k+"a"} d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />, <path key={k+"b"} d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />],
+  "file-text": (k) => [<path key={k+"a"} d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />, <polyline key={k+"b"} points="14 2 14 8 20 8" />, <line key={k+"c"} x1="16" y1="13" x2="8" y2="13" />, <line key={k+"d"} x1="16" y1="17" x2="8" y2="17" />, <polyline key={k+"e"} points="10 9 9 9 8 9" />],
 };
 
 const Icon = ({ name, size = 16, color = "currentColor", strokeWidth = 1.75, style = {} }) => {
