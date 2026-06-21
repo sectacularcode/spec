61663993d58d86e2d86d4b2fa20a82a079dfb528
sectacularcode <admin@editorialvibes.com>
2026-06-20 20:57:03 -0400

    Add shared htmlEscape utility to src/utils/

---
+++ src/utils/htmlEscape.js
@@ -0,0 +1,12 @@
+// src/utils/htmlEscape.js
+// Shared HTML escape utility used by both Template Studio and Brief to Blueprint.
+
+export function he(s) {
+  if (s == null) return "";
+  return String(s)
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
+}
